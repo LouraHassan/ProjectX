526bb21fae32aa72615ff94d3616a6c9acacbe09
Lomooon <Alfarsi.loura@gmail.com>
2025-12-06 11:13:26 +0300

    interactive page

--- script.js
+++ script.js
@@ -3,5 +3,37 @@ const welcomeMessage = 'welcome to my site'
 
 console.log(welcomeMessage);
 
+//get element by id
+//get elements by class name
+//queryselector
+
+const title = document.getElementById('title-1')
+console.log(title);
+
+const text = document.getElementsByClassName('title-2')
+console.log(text[1]);
+
+
+
+const list = document.getElementById('list')
+const input = document.getElementById('input')
+const button = document.getElementById('button')
+
+const onClick =  () => {
+    const item = document.createElement("li")
+    item.innerHTML = input.value
+    list.appendChild(item)
+    input.value = ''
+}
+button.addEventListener("click", onClick)
+
+const colorInput = document.getElementById('color-input')
+const colorBtn = document.getElementById('color-btn')
+
+const body = document.body
+colorBtn.addEventListener('click', () => {
+    body.style.backgroundColor = colorInput.value
+})
+
 
 
