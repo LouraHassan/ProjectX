ca461e1ea8e53482811e1f07f4373acef4d908c7
Lomooon <Alfarsi.loura@gmail.com>
2025-12-06 12:07:48 +0300

    arrays and loops

--- script.js
+++ script.js
@@ -1,39 +1,36 @@
 
 const welcomeMessage = 'welcome to my site'
 
-console.log(welcomeMessage);
-
-//get element by id
-//get elements by class name
-//queryselector
-
+const name = 'loura'
 const title = document.getElementById('title-1')
-console.log(title);
-
+title.innerHTML = `My name is ${name} `
 const text = document.getElementsByClassName('title-2')
-console.log(text[1]);
-
-
-
 const list = document.getElementById('list')
 const input = document.getElementById('input')
 const button = document.getElementById('button')
 
+input.setAttribute('type','number')
 const onClick =  () => {
-    const item = document.createElement("li")
-    item.innerHTML = input.value
-    list.appendChild(item)
+
+    list.innerHTML += `<li>${input.value}</li>`
     input.value = ''
 }
 button.addEventListener("click", onClick)
 
 const colorInput = document.getElementById('color-input')
 const colorBtn = document.getElementById('color-btn')
-
 const body = document.body
 colorBtn.addEventListener('click', () => {
     body.style.backgroundColor = colorInput.value
 })
+
+const fruits = ['Apple', "banana", "Mango",'Apple', "banana", "Mango"]
+const fruitList = document.getElementById('fruit-list')
+fruits.forEach(fruit => {
+    fruitList.innerHTML += `<li>${fruit}</li>`
+})
+
+fruitList.setAttribute('type','A')
 
 
 
